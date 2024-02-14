dce7119c4596ff2dc4abe972b4094d496fb4cbc7
ijayanra <jayanraj.ilangovane@ynov.com>
2024-02-14 14:32:43 +0100

    freeze obstacles mouvement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+
     [SerializeField]
     private float _forwardSpeed;
     [SerializeField]
@@ -37,15 +37,15 @@ public class PlayerMovement : MonoBehaviour
         tempSpeed = transform.forward * _forwardSpeed;
 
         //On conserve la vitesse verticale
-        tempSpeed.y =_rb.velocity.y;    
+        tempSpeed.y = _rb.velocity.y;
 
         // Mouvement horizontal
         if (Input.GetKey(KeyCode.RightArrow))
             tempSpeed += transform.right * _lateralSpeed;
         if (Input.GetKey(KeyCode.LeftArrow))
             tempSpeed += -transform.right * _lateralSpeed;
-       
-        _rb.velocity = Vector3.Lerp(_rb.velocity,tempSpeed, _acceleration * Time.deltaTime) ;
+
+        _rb.velocity = Vector3.Lerp(_rb.velocity, tempSpeed, _acceleration * Time.deltaTime);
 
         //Mouvement de saut
         if (Input.GetKeyDown(KeyCode.Space) && _numberOfColliderUnder > 0 && _timer > 1.0f)
@@ -53,7 +53,7 @@ public class PlayerMovement : MonoBehaviour
             _rb.AddForce(new Vector3(0, _jumpForce, 0));
             _playerAnimator.SetTrigger("Jump");
         }
-        
+
         //Mouvement de slide
         // en QWERTY Z = W
         if (Input.GetKeyDown(KeyCode.Z) && _numberOfColliderUnder > 0 && _timer > 1.0f)
@@ -64,7 +64,7 @@ public class PlayerMovement : MonoBehaviour
         //Gravité
         if (_rb.velocity.y < -1)
             _rb.AddForce(Physics.gravity * Time.deltaTime * 100);
-            
+
     }
 
     private void OnTriggerEnter(Collider other)
